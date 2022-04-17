7ca950b4698c6d2422b53bd140d92c63c2368f51
together08 <together08@yeah.net>
2022-04-17 13:39:07 +0800

    m2181: Build radio hals

--- m2181.mk
+++ m2181.mk
@@ -309,6 +309,9 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
     libprotobuf-cpp-full \
     librmnetctl \
     libxml2
