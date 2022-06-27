20309644acf74bde38f2651133e75fab2c2d6cfd
together08 <together08@yeah.net>
2022-06-27 16:07:07 +0800

    m2181: Build fingerprint 2.3 service

--- m2181.mk
+++ m2181.mk
@@ -193,6 +193,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
