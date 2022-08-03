273e8abb004ace7f0e35d6b4d33b073d825f5181
together08 <together08@yeah.net>
2022-08-03 21:44:03 +0800

    m2181: Arrow-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m2181.mk
+    $(LOCAL_DIR)/arrow_m2181.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_m2181-eng
+    arrow_m2181-eng
--- arrow_m2181.mk
+++ arrow_m2181.mk
@@ -5,10 +5,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from m2181 device
 $(call inherit-product, device/meizu/m2181/m2181.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
-PRODUCT_NAME := lineage_m2181
+PRODUCT_NAME := arrow_m2181
 PRODUCT_DEVICE := m2181
 PRODUCT_BRAND := meizu
 PRODUCT_MODEL := meizu18
